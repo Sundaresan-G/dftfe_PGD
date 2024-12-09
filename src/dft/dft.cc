11adafc3af657500029b300dc918379c08e51f3d
Srinibas Nandi <srinibasn@iisc.ac.in>
2024-12-09 22:25:14 +0530

    d_numEigenValues changed for NSCF

--- src/dft/dft.cc
+++ src/dft/dft.cc
@@ -532,13 +532,13 @@ namespace dftfe
       pcout << "Setting netcharge " << d_dftParamsPtr->netCharge << std::endl;
 
     if (d_dftParamsPtr->solverMode == "NSCF" &&
-        d_dftParamsPtr->numberEigenValues == 0 &&
         d_dftParamsPtr->highestStateOfInterestForChebFiltering != 0)
       {
-        d_numEigenValues =
+        d_numEigenValues = std::max(
+          static_cast<double>(d_dftParamsPtr->numberEigenValues),
           std::max(d_dftParamsPtr->highestStateOfInterestForChebFiltering * 1.1,
                    d_dftParamsPtr->highestStateOfInterestForChebFiltering +
-                     10.0);
+                     10.0));
         if (d_dftParamsPtr->verbosity >= 1)
           {
             pcout
